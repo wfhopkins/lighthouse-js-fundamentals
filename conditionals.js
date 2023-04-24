70128456d25e67b34d9bd1fae05b3e923a42b2be
wfhopkins <will.hop@gmail.com>
2023-04-24 14:23:23 -0700

    using conditionals to tell people about the weataher

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const raining = true;
+/*const raining = true;
 const cold = true;
 
 if (raining) {
@@ -9,4 +9,24 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
+console.log("Now you're ready to go outside!");
+
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+*/
+const temperature = 20;
+
+if (temperature < 0){
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15){
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
 console.log("Now you're ready to go outside!");
