6bd72355cc504b363d94192b1a64939665728a08
wfhopkins <will.hop@gmail.com>
2023-04-30 18:56:30 -0700

    working on for and for...of loops to say hello and add sums

---
+++ tinkering.js
@@ -0,0 +1,17 @@
+/*const amounts = [61.00, 52.25, 112.99, 5.00];
+let total = 0;
+ //using for loops
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+//using for...of loop
+for (let amount of amounts) {
+  total += amount;
+}
+console.log("Order total is: ", total);
+*/
+function sayHello() {
+  var message = "Hello!";
+  return message;
+}
+console.log(sayHello());
