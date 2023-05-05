104e5307b16877b5bde8d0c965842191cd51c3f1
wfhopkins <will.hop@gmail.com>
2023-05-05 16:20:37 -0700

    working on function expressions inside anonymous functions

--- hello.js
+++ hello.js
@@ -1,7 +1,13 @@
+//declaring function to sayHello(name)
 const sayHello = function (name) {
   console.log("Hello, " + name);
-}
-
+};
+/*
 sayHello("Hamlet");
 sayHello("Ophelia");
 sayHello("Horatio");
+ */
+const returnSayHello = function(name){
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
