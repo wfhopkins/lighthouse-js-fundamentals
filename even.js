c6fb970a24c51810f868f2d68e24c2e5375be41a
wfhopkins <will.hop@gmail.com>
2023-05-05 16:39:44 -0700

    creating isEven function to check if num variable is divisible by two

---
+++ even.js
@@ -0,0 +1,8 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+//const tenIsEven = isEven(10);
+//console.log(tenIsEven);
+console.log(isEven(10));
+console.log(isEven(13));
+
--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-//declaring function to sayHello(name)
+/* //declaring function to sayHello(name)
 const sayHello = function (name) {
   console.log("Hello, " + name);
 };
@@ -6,8 +6,23 @@ const sayHello = function (name) {
 sayHello("Hamlet");
 sayHello("Ophelia");
 sayHello("Horatio");
- */
+//
 const returnSayHello = function(name){
   return "Hello, " + name;
 }
-const greeting = returnSayHello('John');
+const greeting = returnSayHello('John'); */
+//
+//working on using return statements in addition to console.log
+/*
+const sayHelloToConsole = function (name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+*/
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+
+console.log(greeting);
+
