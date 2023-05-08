b5ea59ca4c7fe7459c9b9ae7da981724c9b87101
wfhopkins <will.hop@gmail.com>
2023-05-07 17:56:25 -0700

    using arrays to adjust ice cream flavours and starting the packing list problem

---
+++ packing-list.js
@@ -0,0 +1,4 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
