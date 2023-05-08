10b150499a37e76b9aafd7db7f12fbb5d2a5bf4f
wfhopkins <will.hop@gmail.com>
2023-05-07 21:07:17 -0700

    using for and whilte loops to print data to the console from an array

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-for (let i = 0; i < packingList.length; i++) {
+//packing list using for loop
+/*for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+} */
+//
+//packing list using while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
